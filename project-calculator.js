e2a4c709d1744ebbd9db09b592d8f440c78bf123
crypto.schmoi <tayla.akurangi@gmail.com>
2023-09-18 22:43:02 +1000

    Created variables & operations for the calculator
    - 3 variables were created to represent the first number, operation type and second number.
    
    - A function called "operate" was made to call on the variables and operation types

--- project-calculator.js
+++ project-calculator.js
@@ -1,20 +1,36 @@
 // Creates a function for addition  on the calculator
-const add = function(a, b) {
-    return a + b;
-  };
-  
+const add = function (a, b) => a + b;
 
 // Creates a function for subtraction on the calculator
-  const subtract = function(a, b) {
-      return a - b;
-  };
+const subtract = function (a, b) => a - b;
   
 // Creates a function for multiplication on the calculator
-  const multiply = function(a, b) {
-      return a * b;
-    };
+const multiply = function (a, b) => a * b;
 
 // Creates a function for division on the calculator
-const division = function (a, b) {
-    return a / b;
+const divide = function (a, b) => a / b;
+
+// Variables for the calulcator operation
+let firstNumber = "";
+
+let operationType = "";
+
+let secondNumber = "";
+
+// Write an equation that is firstNumber, operator, secondNumber
+const operate = function (firstNumber, operationType, secondNumber) {
+    if (operationType === "+") {
+        return (firstNumber + secondNumber);
+    } else if (operationType === "-") {
+        return (firstNumber - secondNumber); 
+    } else if (operationType === "*") {
+        return (firstNumber * secondNumber); 
+    } else if (operationType === "/") {
+        return (firstNumber / secondNumber);
+    }
 }
+
+- a function that takes the 2 numbers (from the variable) and applies 
+the operation to it (call the new function "operate")
+
+
