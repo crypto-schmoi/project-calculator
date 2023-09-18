0b606c1d8f7629b226c80d9e709ae6ca32540f99
crypto.schmoi <tayla.akurangi@gmail.com>
2023-09-18 21:59:56 +1000

    Create functions for operations on the calculator
    Functions were created for addition, subtraction, multiplcation and division.
    These functions represent the operations that will be used in the calculator.

---
+++ project-calculator.js
@@ -0,0 +1,20 @@
+// Creates a function for addition  on the calculator
+const add = function(a, b) {
+    return a + b;
+  };
+  
+
+// Creates a function for subtraction on the calculator
+  const subtract = function(a, b) {
+      return a - b;
+  };
+  
+// Creates a function for multiplication on the calculator
+  const multiply = function(a, b) {
+      return a * b;
+    };
+
+// Creates a function for division on the calculator
+const division = function (a, b) {
+    return a / b;
+}
