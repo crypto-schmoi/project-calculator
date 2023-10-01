631f83e0be790503db99dc75d0e6d69869d0ee22
crypto.schmoi <tayla.akurangi@gmail.com>
2023-10-01 22:23:43 +1000

    Button functions added to Javascript
    - Variables for the button numbers 0 - 9 and operational buttons created.
    - A variable was created to store the display value and track digits being entered.
    - Event listeners for each button were created so when a button is clicked, it will update on the calculator display.

--- project-calculator.js
+++ project-calculator.js
@@ -1,14 +1,22 @@
 // Creates a function for addition  on the calculator
-const add = function (a, b) => a + b;
+const add = function (a, b) {
+    return a + b;
+}
 
 // Creates a function for subtraction on the calculator
-const subtract = function (a, b) => a - b;
+const subtract = function (a, b) {
+    return a - b;
+}
   
 // Creates a function for multiplication on the calculator
-const multiply = function (a, b) => a * b;
+const multiply = function (a, b) {
+    return a * b;
+} 
 
 // Creates a function for division on the calculator
-const divide = function (a, b) => a / b;
+const divide = function (a, b) {
+    return a / b;
+} 
 
 // Variables for the calulcator operation
 let firstNumber = "";
@@ -17,8 +25,9 @@ let operationType = "";
 
 let secondNumber = "";
 
-// Write an equation that is firstNumber, operator, secondNumber
-const operate = function (firstNumber, operationType, secondNumber) {
+
+// Equation that calls the function when a particular operation type is selected
+let operate = function (firstNumber, operationType, secondNumber) {
     if (operationType === "+") {
         return (firstNumber + secondNumber);
     } else if (operationType === "-") {
@@ -30,7 +39,78 @@ const operate = function (firstNumber, operationType, secondNumber) {
     }
 }
 
-- a function that takes the 2 numbers (from the variable) and applies 
-the operation to it (call the new function "operate")
+// Create the functions that populate the display when you click the 
+// number buttons. You should be storing the ‘display value’ in a variable 
+// somewhere for use in the next step.
 
+const display = document.querySelector('display');
 
+// Variables for the buttons on the calculator
+const zeroButton = document.querySelector('.zero');
+const oneButton = document.querySelector('.one');
+const twoButton = document.querySelector('.two');
+const threeButton = document.querySelector('.three');
+const fourButton = document.querySelector('.four')
+const fiveButton = document.querySelector('.five');
+const sixButton = document.querySelector('.six');
+const sevenButton = document.querySelector('.seven');
+const eightButton = document.querySelector('.eight');
+const nineButton = document.querySelector('.nine');
+const equalsButton = document.querySelector('.equals');
+const additionButton = document.querySelector('.addition');
+const subtractButton = document.querySelector('.subtraction');
+const multiplyButton = document.querySelector('.multiplication');
+const divisionButton = document.querySelector('.division');
+const clearButton = document.querySelector('.clear');
+
+// Variable for storing the display value to track digits being entered
+let displayValue = "";
+
+function updateDisplay() {
+    return displayValue;
+}
+
+// Event listeners for the buttons on the calculator to update the display when a button is clicked
+zeroButton.addEventListener('click', function() {
+    displayValue += '0';
+});
+
+oneButton.addEventListener('click', function() {
+    displayValue += '1';
+});
+
+twoButton.addEventListener('click', function() {
+    displayValue += '2';
+});
+
+threeButton.addEventListener('click', function() {
+    displayValue += '3';
+});
+
+fourButton.addEventListener('click', function() {
+    displayValue += '4';
+});
+
+fiveButton.addEventListener('click', function() {
+    displayValue += '5';
+});
+
+sixButton.addEventListener('click', function() {
+    displayValue += '6';
+});
+
+sevenButton.addEventListener('click', function() {
+    displayValue += '7';
+});
+
+eightButton.addEventListener('click', function() {
+    displayValue += '8';
+});
+
+nineButton.addEventListener('click', function() {
+    displayValue += '9';
+});
+
+equalsButton.addEventListener('click', function() {
+    return operate;
+});
